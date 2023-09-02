a5b2f4bd967fe7991e05d41c36e6ba54fb95f58b
rishavcool21 <kumarrishavcool21@gmail.com>
2023-09-02 20:35:13 +0530

    Added Week2

--- customizing-example/src/App.js
+++ customizing-example/src/App.js
@@ -4,15 +4,17 @@ import Btn from './Btn';
 import ModeToggler from './ModeToggler';
 import GuessNum from './GuessNum';
 import Child from "./Child"
+import RegisterForm from './RegisterForm';
 
 const date = new Date()
 
 function App() {
   return (
     <div className="App">
-      <Child message={date.toLocaleTimeString()}/>
+      <RegisterForm/>
+      {/* <Child message={date.toLocaleTimeString()}/>
       <Child message={date.toLocaleDateString()}/>
-      {/* <ModeToggler/>
+      <ModeToggler/>
        <h1>Task: Add three Card elements</h1>
       <Card h2="First card's h2" h3="First card's h3"/>
       <Card h2="Second card's h2" h3="Second card's h3"/>
---
+++ week2/src/MealsList.jsx
@@ -0,0 +1,7 @@
+export default function MealsList() {
+    return (
+        <div>
+
+        </div>
+    )
+}
