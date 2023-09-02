4f5b80c9a733f279323f634e27541e1358475c41
rishavcool21 <kumarrishavcool21@gmail.com>
2023-09-02 16:54:16 +0530

    Added Time by using children and data

--- customizing-example/src/App.js
+++ customizing-example/src/App.js
@@ -3,16 +3,22 @@ import Card from './Card';
 import Btn from './Btn';
 import ModeToggler from './ModeToggler';
 import GuessNum from './GuessNum';
+import Child from "./Child"
+
+const date = new Date()
+
 function App() {
   return (
     <div className="App">
-      <ModeToggler/>
+      <Child message={date.toLocaleTimeString()}/>
+      <Child message={date.toLocaleDateString()}/>
+      {/* <ModeToggler/>
        <h1>Task: Add three Card elements</h1>
       <Card h2="First card's h2" h3="First card's h3"/>
       <Card h2="Second card's h2" h3="Second card's h3"/>
       <Card h2="Third card's h2" h3="Third card's h3"/>
       <Btn />
-      <GuessNum/>
+      <GuessNum/> */}
     </div>
   );
 }
