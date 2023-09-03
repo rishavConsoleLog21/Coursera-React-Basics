ccc39c9658b291d448e729302320eb025d1cc8db
rishavcool21 <kumarrishavcool21@gmail.com>
2023-09-03 18:38:44 +0530

    Added Image in ReactApp

--- week3/src/App.js
+++ week3/src/App.js
@@ -1,23 +1,32 @@
-import './App.css';
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
-import { Routes ,Route, Link } from 'react-router-dom';
+import "./App.css";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-            <nav className='nav'>
-                <Link to="/" className='nav-item'>Home</Link>
-                <Link to="/about" className='nav-item'>About</Link> 
-                <Link to="/contact" className='nav-item'>Contact Us</Link> 
-            </nav>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-    </div>
+    <>
+      <div className="App">
+        <nav className="nav">
+          <Link to="/" className="nav-item">
+            Home
+          </Link>
+          <Link to="/about" className="nav-item">
+            About
+          </Link>
+          <Link to="/contact" className="nav-item">
+            Contact Us
+          </Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+     
+    </>
   );
 }
 
--- week3/src/Home.js
+++ week3/src/Home.js
@@ -1,10 +1,25 @@
+import camp from "./assets/images/camp.jpg";
+
 function Home() {
+  const randomImageUrl = "https://source.unsplash.com/random/800x600";
   return (
     <div>
       <h1>Home</h1>
       <p>This is the home page</p>
+      <div>
+        <h1>Camping With Us</h1>
+        <img height={200} src={camp} alt="camping" />
+      </div>
+      {/* <div>
+        <h1>Camping With Us</h1>
+        <img height={200} src={require("./assets/images/camp.jpg")} alt="camping" />
+      </div> */}
+      <div>
+        <h1>Random Image From Unsplasg</h1>
+        <img height={200} src={randomImageUrl} alt="camping" />
+      </div>
     </div>
   );
 }
-
+//above are the 3 example of how to use image in react
 export default Home;
