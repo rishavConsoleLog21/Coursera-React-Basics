878d07c67da47c75b634d3cfc0475b42874e8548
rishavcool21 <kumarrishavcool21@gmail.com>
2023-09-04 00:32:43 +0530

    Added Video on raact about page

--- week3/src/About.js
+++ week3/src/About.js
@@ -1,8 +1,10 @@
+import Video from "./Video";
 function About() {
   return (
     <div>
         <h1>About</h1>
         <p>This is the about page</p>
+        <Video/>
     </div>
   )
 }
---
+++ week3/src/Video.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+const containerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '50vh', // Optional: This centers content vertically in the viewport
+  };
+
+const Video = () => {
+    return (
+        <div>
+            <MyVideo/>
+        </div>
+    )
+};
+
+const MyVideo = () => {
+    return (
+        <div style={containerStyle}>
+            <div>
+            <ReactPlayer url='https://www.youtube.com/watch?v=cVA-9JHwbFY' />
+            </div>
+        </div>
+    )
+}
+
+export default Video;
