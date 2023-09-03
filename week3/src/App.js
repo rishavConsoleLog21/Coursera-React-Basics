644bc6fa8aac7bdbd76a1808d15c0ba192e4bb1b
rishavcool21 <kumarrishavcool21@gmail.com>
2023-09-03 16:34:55 +0530

    Added ContactPage

--- week3/src/App.js
+++ week3/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './Home';
 import About from './About';
+import Contact from './Contact';
 import { Routes ,Route, Link } from 'react-router-dom';
 
 function App() {
@@ -9,10 +10,12 @@ function App() {
             <nav className='nav'>
                 <Link to="/" className='nav-item'>Home</Link>
                 <Link to="/about" className='nav-item'>About</Link> 
+                <Link to="/contact" className='nav-item'>Contact Us</Link> 
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
     </div>
   );
